5222fa45d067afedf2d68b5c8c788ebae37531bc
DaaniyaalM <daaniyaalmirza@gmail.com>
2023-10-01 00:35:21 -0700

    hello

---
+++ trash.js
@@ -0,0 +1,7 @@
+// .header__overlay {
+//     background-color: rgba(0, 0, 0, 0.501);
+//     width: 100%;
+//     display: flex;
+//     flex-direction: row;
+//     justify-content: center;
+//   }
